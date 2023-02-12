ad4c20728e9847ed002fd9136db12937f4ce5538
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2023-02-12 18:43:43 +0600

    Built-in types

--- src/chapter_2.ts
+++ src/chapter_2.ts
@@ -1,1 +1,32 @@
-let x = 1;
+let x: number = 1;
+let y: string = "String value";
+let z: boolean = true;
+let a: Date = new Date();
+let b: [string] = ["String value"];
+let c: [number] = [1];
+let d: [boolean] = [true];
+let e: [Date] = [new Date()];
+let f: [string, number] = ["String value", 1];
+let g: [string, number, boolean] = ["String value", 1, true];
+let h: [string, number, boolean, Date] = ["String value", 1, true, new Date()];
+
+// array of strings.
+let i: string[] = ["String value", "String value"];
+// array of numbers.
+let j: number[] = [1, 2, 3];
+// array of booleans.
+let k: boolean[] = [true, false, true];
+// array of dates.
+let l: Date[] = [new Date(), new Date()];
+// array of arrays of strings.
+let m: [string][] = [["String value"], ["String value"]];
+// array of arrays of numbers.
+let n: [number][] = [[1], [2], [3]];
+// array of arrays of booleans.
+let o: [boolean][] = [[true], [false], [true]];
+// array of arrays of dates.
+let p: [Date][] = [[new Date()], [new Date()]];
+
+let anyValue: any = "String value";
+anyValue = 1;
+anyValue = true;
