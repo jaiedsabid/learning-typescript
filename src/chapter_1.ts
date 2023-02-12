197c38a3928bce1bbf6d081c98fd4e181bec1a22
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2023-02-12 18:31:16 +0600

    Renamed file index to chapter_1

---
+++ src/chapter_1.ts
@@ -0,0 +1,32 @@
+'use strict';
+class User {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    setName(name: string) {
+        this.name = name;
+    }
+    getName(): string {
+        return this.name;
+    }
+    setAge(age: number) {
+        this.age = age;
+    }
+    getAge(): number {
+        return this.age;
+    }
+}
+
+const user1 = new User('Tom', 24);
+console.log({name: user1.getName(), age: user1.getAge()});
+user1.setAge(25);
+user1.setName('Tom Hanks');
+console.log({name: user1.getName(), age: user1.getAge()});
+
+// JavaScript type checking.
+let nameStr = 'Jaied';
+// nameStr = 24; // Error: Type 'number' is not assignable to type 'string'.
+nameStr = 'Tom';
